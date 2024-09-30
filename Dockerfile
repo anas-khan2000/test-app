dc7a28b25f06934e915097b4ff99355bc661f0ad
anas-khan2000 <mak.bvb@gmail.com>
2024-09-30 17:42:56 +0000

    correction made

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,6 @@ RUN pip3 install flask
 
 COPY app.py /opt/app.py
 
-RUN apt-get update && apt-get install -y python3 python3-pip
-
-COPY app.py /opt/app.py
-
-RUN pip3 install flask
-
 EXPOSE 5000
 
 CMD ["python3", "/opt/app.py"]
--- app.py
+++ app.py
@@ -3,16 +3,9 @@ from flask import Flask
 # Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define a route for the root URL ('/')
-app = Flask(__name__)
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-# Run the app when the script is executed
-if __name__ == '__main__':
-    app.run(host='0.0.0.0' , port=5000)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
